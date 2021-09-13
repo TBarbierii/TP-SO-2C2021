387778d8404adec8eb24b0b97fd448fb8eea4e92
gabycosta04 <ccosta@est.frba.utn.edu.ar>
2021-09-12 21:00:58 -0300

    agrego listas de planificacion

--- Kernel/include/Kernel.h
+++ Kernel/include/Kernel.h
@@ -5,5 +5,20 @@
 #include <commons/log.h>
 #include <stdbool.h>
 #include "shared_utils.h"
+#include <commons/collections/list.h>
+#include <commons/config.h>
+
+t_list* procesosNew;
+t_list* procesosReady;
+t_list* procesosExec;
+t_list* procesosExit;
+t_list* procesosBlockIO;
+t_list* procesosSuspendedBlock;
+t_list* procesosSuspendedReady;
+
+
+
+void inicializarListas();
+void finalizarListas();
 
 #endif
--- Kernel/src/Kernel.c
+++ Kernel/src/Kernel.c
@@ -1,7 +1,42 @@
 #include "Kernel.h"
 
-int main(){
-    t_log* logger = log_create("./cfg/proceso2.log", "PROCESO1", true, LOG_LEVEL_INFO);
-    log_info(logger, "Soy el proceso 2! %s", mi_funcion_compartida());
-    log_destroy(logger);
+
+void inicializarListas(){
+    procesosNew = list_create();
+    procesosReady = list_create();
+    procesosBlockIO = list_create();
+    procesosExec = list_create();
+    procesosExit = list_create();
+    procesosSuspendedReady = list_create();
+    procesosSuspendedBlock = list_create();
 }
+
+void finalizarListas(){
+    list_destroy(procesosNew);
+    list_destroy(procesosReady);
+    list_destroy(procesosExec);
+    list_destroy(procesosBlockIO);
+    list_destroy(procesosExit);
+    list_destroy(procesosSuspendedReady);
+    list_destroy(procesosSuspendedBlock);
+}
+
+
+t_config* inicializarConfig(){
+    return config_create("cfg/ConfiguracionKernel.config");
+}
+
+void finalizarConfig(t_config* configUsado){
+    config_destroy(configUsado);
+}
+
+
+int main(){
+    inicializarListas();
+    t_config* configActual = inicializarConfig();
+    finalizarListas();
+    finalizarConfig(configActual);
+    return 0;
+}
+
+
