a39a6b7f24caf943c3c3279618ee7582ab5c3d29
gabycosta04 <ccosta@frba.utn.edu.ar>
2021-11-06 00:04:33 -0300

    agrego nuevas modificaciones en base a como pidieron que tenia que ser el tema de los THREAD CPU

--- Kernel/src/PlanificadorCortoPlazo.c
+++ Kernel/src/PlanificadorCortoPlazo.c
@@ -1,6 +1,6 @@
 #include "PlanificadorCortoPlazo.h"
 
-
+/* 
 void rutinaDeProceso(proceso_kernel* procesoEjecutando){
     
     clock_t arranqueEjecucion = clock();
@@ -54,6 +54,81 @@ void rutinaDeProceso(proceso_kernel* procesoEjecutando){
     log_destroy(logger);
 
 }
+*/
+
+void rutinaDeProceso(){
+    
+    
+    t_log* logger = log_create("cfg/PlanificadorCortoPlazoActual.log","Thread-CPU", 1, LOG_LEVEL_DEBUG);
+    
+    while(1){
+
+        //como son hilos creados al comienzo, aca deberiamos sacar al primer proceso que este en exec y agregarlo
+        sem_wait(procesosDisponiblesParaEjecutar);
+
+        //vamos a sacar al primero que esta en READY, replanificando por las dudas
+        pthread_mutex_lock(modificarReady);
+            replanificacion(); //en este momento replanifico y ordeno la lista de readys segun el criterio seleccionado
+            proceso_kernel* procesoListoParaEjecutar = list_remove(procesosReady, 0);
+            log_debug(logger, "Se saca un carpincho de la lista de readys para ejecutar. Carpincho: %d",procesoListoParaEjecutar->pid);
+        pthread_mutex_unlock(modificarReady);
+    
+        //lo ponemos en la lista de exec
+        pthread_mutex_lock(modificarExec);
+            list_add(procesosExec, procesoListoParaEjecutar);
+            log_debug(logger, "Se agrega un nuevo carpincho a la lista de carpinchos en ejecucion. Carpincho: %d", procesoListoParaEjecutar->pid);
+        pthread_mutex_unlock(modificarExec);
+        
+        //le ponemos el momento donde entro en EXEC en este THREAD:
+        clock_t arranqueEjecucion = clock();
+
+        while(1){
+
+            
+            //si viene de un bloqueo debemos notificarle a la matelib que se pudo o no realizar
+            if(procesoListoParaEjecutar->vuelveDeBloqueo == BLOCK_IO){
+
+                //le avisamos que se pudo realizar la IO
+                log_info(logger, "Le avisamos al proceso que se pudo ejecutar el IO correctamente");
+                int valor = 0;
+                avisarconexionConDispositivoIO(procesoListoParaEjecutar->conexion, valor);
+
+            }else if (procesoListoParaEjecutar->vuelveDeBloqueo == BLOCK_SEM){
+                //le avisamos que se pudo realizar el WAIT
+                log_info(logger, "Le avisamos al proceso que se pudo ejecutar el SEM_WAIT correctamente");
+                int valor = 0;
+                avisarWaitDeSemaforo(procesoListoParaEjecutar->conexion, valor);
+            }
+            
+            
+            log_info(logger, "Se ejecuto tarea de conexion");
+            int codigoOperacion = atenderMensajeEnKernel(procesoListoParaEjecutar->conexion);
+            log_info(logger, "La tarea realizada fue: %d", codigoOperacion);
+
+            if(rompoElHiloSegunElCodigo(codigoOperacion) == 1){
+
+                log_info(logger, "Se realizo una operacion que termina con la ejecucion del Carpincho por el momento para bloquearlo");
+                clock_t finEjecucion = clock();
+                procesoListoParaEjecutar->ultimaRafagaEjecutada = (double)(finEjecucion - arranqueEjecucion) / CLOCKS_PER_SEC;
+                calcularEstimacion(procesoListoParaEjecutar); //aaca calculo la ultima estimacion nueva en base a la ultima rafaga ejecutada
+                sem_post(nivelMultiprocesamiento);
+                break;
+
+            }else if(rompoElHiloSegunElCodigo(codigoOperacion) == 2){
+                
+                log_info(logger, "Se realizo una operacion que termina con la ejecucion del Carpincho");
+                break;
+
+            }
+
+        }
+
+    }
+
+    log_destroy(logger);
+
+}
+
 
 
 
@@ -61,33 +136,28 @@ void planificadorCortoPlazo(){
 
     t_log* logger = log_create("cfg/PlanificadorCortoPlazoActual.log","PlanificadorCortoPlazo", 0, LOG_LEVEL_DEBUG);
     log_debug(logger, "El algoritmo utilizado para planificar en el Corto Plazo sera: %s",algoritmoPlanificacion);
-  
+    inicializarHilosCPU();
     while(1){
     
         sem_wait(hayProcesosReady);
         sem_wait(nivelMultiprocesamiento);
-        
-        pthread_mutex_lock(modificarReady);
-
-            replanificacion(); //en este momento replanifico y ordeno la lista de readys segun el criterio seleccionado
-            proceso_kernel* procesoListoParaEjecutar = list_remove(procesosReady, 0);
-            log_debug(logger, "Se saca un carpincho de la lista de readys para ejecutar. Carpincho: %d",procesoListoParaEjecutar->pid);
-        pthread_mutex_unlock(modificarReady);
-    
-        pthread_mutex_lock(modificarExec);
-            list_add(procesosExec, procesoListoParaEjecutar);
-            log_debug(logger, "Se agrega un nuevo carpincho a la lista de carpinchos en ejecucion. Carpincho: %d", procesoListoParaEjecutar->pid);
-        pthread_mutex_unlock(modificarExec);
-
-        pthread_t hiloDeEjecucion;
-        log_debug(logger, "Se crea un hilo para el carpincho. Carpincho: %d", procesoListoParaEjecutar->pid);
-        pthread_create(&hiloDeEjecucion, NULL, (void *) rutinaDeProceso, procesoListoParaEjecutar);
-        pthread_detach(hiloDeEjecucion);
-
+        //esto lo unico que va a hacer es avisarle a los hilos que hay un nuevo proceso disponible y que lo ponga en ejecucion
+        sem_post(procesosDisponiblesParaEjecutar);
     }
 
     log_destroy(logger);
 
+}
+
+void inicializarHilosCPU(){
+    for (int i = 0; i < gradoMultiProcesamiento ; i++)
+    {
+        pthread_t hiloDeEjecucion;
+        pthread_create(&hiloDeEjecucion, NULL, (void *) rutinaDeProceso, NULL);
+        pthread_detach(hiloDeEjecucion);
+    }
+    
+    
 }
 
 
