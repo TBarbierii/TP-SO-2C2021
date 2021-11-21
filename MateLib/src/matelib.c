fac13ac19d3e688ecda934af2dd93518646abeac
santiagocorbalan <scorbalan@frba.utn.edu.ar>
2021-11-21 12:51:15 -0300

    modificaciones en la matelib

--- MateLib/src/matelib.c
+++ MateLib/src/matelib.c
@@ -1,5 +1,122 @@
 #include "matelib.h"
 
+uint32_t iniciar_servidor(char* ip_servidor, char* puerto)
+{
+	int socket_servidor;
+
+	struct addrinfo hints, *servinfo;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+
+    getaddrinfo(ip_servidor, puerto, &hints, &servinfo);
+
+    socket_servidor = socket(servinfo->ai_family, 
+                         servinfo->ai_socktype, 
+                         servinfo->ai_protocol);
+
+	bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+	listen(socket_servidor, SOMAXCONN);
+
+    freeaddrinfo(servinfo);
+
+    return socket_servidor;
+}
+
+
+
+
+
+int esperar_cliente(int socket_servidor)
+{
+	
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
+
+	return socket_cliente;
+}
+
+//atender solicitudes deberia estar en cada modulo
+
+int crear_conexion(char *ip, char* puerto)
+{
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo(ip, puerto, &hints, &server_info);
+
+	int socket_cliente = socket(server_info->ai_family,
+								server_info->ai_socktype,
+								server_info->ai_protocol);
+
+    if(socket_cliente==-1){
+        perror("No se pudo crear la conexion");
+        return -1;
+    }
+
+	int conexion = connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+	freeaddrinfo(server_info);
+	if(conexion == -1){
+		return conexion;
+	}
+	return socket_cliente;
+}
+
+
+
+void* serializar_paquete(t_paquete* paquete, int bytes)
+{	
+	void * contenido_serializado = malloc(bytes);
+	
+	int desplazamiento = 0;
+	
+	memcpy(contenido_serializado + desplazamiento, &(paquete->codigo_operacion), sizeof(uint32_t));
+	desplazamiento+= sizeof(uint32_t);
+	
+	memcpy(contenido_serializado + desplazamiento, &(paquete->buffer->size), sizeof(uint32_t));
+	desplazamiento+= sizeof(uint32_t);
+	
+	memcpy(contenido_serializado + desplazamiento, paquete->buffer->stream, paquete->buffer->size);
+	desplazamiento+= paquete->buffer->size;
+
+	return contenido_serializado;
+}
+
+
+void crear_buffer(t_paquete* paquete)
+{
+	paquete->buffer = malloc(sizeof(t_buffer));
+	paquete->buffer->size = 0;
+	paquete->buffer->stream = NULL;
+}
+
+
+t_paquete* crear_paquete(cod_operacion codigo)
+{
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->codigo_operacion = codigo;
+	crear_buffer(paquete);
+	return paquete;
+}
+
+
+void enviarPaquete(t_paquete* paquete, int conexion){
+
+    int bytes = paquete->buffer->size + sizeof(cod_operacion) + sizeof(uint32_t);
+    void* contenido_a_enviar= serializar_paquete(paquete, bytes);
+    send(conexion, contenido_a_enviar,bytes,0);
+    free(contenido_a_enviar);
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+
+}
 
 
 //------------------General Functions---------------------/
--- MateLib/src/matelib.h
+++ MateLib/src/matelib.h
@@ -7,13 +7,93 @@
 #include <string.h>
 #include <stdlib.h>
 #include <commons/log.h>
-#include "shared_utils.h"
 #include <commons/config.h>
 #include <commons/string.h>
+#include <commons/collections/list.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <netdb.h>
 
+typedef enum tipo_bloqueo{
+	NO_BLOQUEADO = -1,
+	BLOCK_SEM,
+	BLOCK_IO
+}tipo_bloqueo;
+
+
+typedef struct proceso{
+    
+    uint32_t pid ;
+    int conexion; // es el socket del proceso
+	int conexionConMemoria; //socket para las tareas que necesitemos la memoria
+    double tiempoDeEspera; // para HRRN
+    double ultimaRafagaEjecutada ; // este es el real Anterior para SJF
+    double rafagaEstimada; //para JFS
+    double responseRatio; // HRRN
+	int vuelveDeBloqueo; //esto lo vamos a utilizar para ver si lo utlimo que realizo fue un bloqueo o no
+	t_list* listaRecursosRetenidos;
+	t_list* listaRecursosSolicitados;
+    clock_t tiempoDeArriboColaReady; //esto nos va a servir cuando queremos calcular el tiempo que estuvo esperando un proceso en la cola de Ready, donde esta variable va a ser el inicio de cuando entro a ready
+
+}proceso_kernel ;
+
+
+
+
+t_log* logger;
+
+typedef enum backend{
+
+    ERROR = -1,
+    OK = 0
+
+}backend;
+
+
+typedef enum{
+	INICIALIZAR_ESTRUCTURA,
+	CERRAR_INSTANCIA,
+	INICIAR_SEMAFORO,
+	SEM_WAIT,
+	SEM_SIGNAL,
+	CERRAR_SEMAFORO,
+	CONECTAR_IO,
+	MEMALLOC,
+	MEMFREE,
+	MEMREAD,
+	MEMWRITE,
+	ESCRITURA_PAGINA,
+	LECTURA_PAGINA,
+	TIPOASIGNACION,
+	SEM_WAIT_NOBLOQUEANTE,
+	SUSPENSION_PROCESO, //este codigo va a ser por el cual desde el kernel le vamos a avisar que un proceso se suspende y vamos a solicitar suspender todas sus paginas
+	CONSULTAR_ESPACIO,
+	FALLO_IO,
+	FINALIZAR_PROCESO
+}cod_operacion;
+
+typedef struct buffer
+{
+	uint32_t size;
+	void* stream;
+} t_buffer;
+
+typedef struct paquete
+{
+	cod_operacion codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+
+
+uint32_t iniciar_servidor(char* ip_servidor, char* puerto);
+int esperar_cliente(int socket_servidor);
+int crear_conexion(char *ip, char* puerto);
+void* serializar_paquete(t_paquete* paquete, int bytes);
+void crear_buffer(t_paquete* paquete);
+t_paquete* crear_paquete(cod_operacion codigo);
+void enviarPaquete(t_paquete* paquete, int conexion);
 
 
 
