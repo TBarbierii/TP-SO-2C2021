1e2e95c72f217b1dbe27ac8db56b6e363dfeeb4e
santiagocorbalan <scorbalan@frba.utn.edu.ar>
2021-11-26 08:54:25 -0300

    subo deploy, falta que clone la carpecha de los carpinchos de prueba y los compile. El LD_LIBRARY_PATH no hace falta

--- prueba-carpinchos/build/.bashrc
+++
@@ -1,2 +0,0 @@
-LD_LIBRARY_PATH=/home/utnso/tp-2021-2c-UCM-20-SO/MateLib/src
-CONFIG= /home/utnso/tp-2021-2c-UCM-20-SO/MateLib/cfg
--- supercarpincho.sh
+++ supercarpincho.sh
@@ -11,16 +11,25 @@ cd $COMMONS
 sudo make uninstall
 make all
 sudo make install
-
-echo -e "\n\n Instalando las pruebas de carpinchoide... \n\n"
-git clone "https://github.com/sisoputnfrba/carpinchos-pruebas.git"
+ls -a
 PRUEBACARPINCHO="carpinchos-pruebas"
+echo -e "\n\n Instalando las pruebas de carpinchoide... \n\n"
+git clone "https://github.com/sisoputnfrba/carpinchos-pruebas.git" $PRUEBACARPINCHO
+
 
 
 cd $CWD
-echo -e "\n\nBuilding projects...\n\n"
+echo -e "\n\nCompilando el proyecto...\n\n"
 make -C ./SWAmP
 make -C ./Memoria
 make -C ./Kernel
 make -C ./MateLib
+
+echo -e "\n\n Copio la matelib a /usr/lib"
+sudo cp -u MateLib/src/libmate.so /usr/lib
+
+echo -e "\n\n Compilar los carpinchos de prueba\n\n"
+cd $PRUEBACARPINCHO
+make compile
+ls
 echo -e "\n\nDale boooooo, dale boooo!\n\n"
